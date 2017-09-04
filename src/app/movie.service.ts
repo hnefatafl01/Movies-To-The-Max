41078fb5345269c7d839f8171ebd89c954563c07
Kevin Erickson <kevin.erick01@gmail.com>
2017-09-04 10:47:40 -0600

    content-type header changed

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -9,7 +9,7 @@ export class MovieService {
   private filter = new Subject<any>();
   private searchSubject = new Subject<any>();
   private API_URL = 'https://mtmserver.herokuapp.com';
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({'Content-Type': 'text/html; charset=UTF-8'});
 
   constructor(private http: Http) {}
 
