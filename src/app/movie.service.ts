4a1894dd1f1adc98d18eba993c751281dc6a98eb
Kevin Erickson <kevin.erick01@gmail.com>
2017-09-04 12:14:43 -0600

    change headers

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -9,7 +9,7 @@ export class MovieService {
   private filter = new Subject<any>();
   private searchSubject = new Subject<any>();
   private API_URL = 'https://mtmserver.herokuapp.com';
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({'Accept' : '*/*', 'Content-Type': 'application/json'});
 
   constructor(private http: Http) {}
 
